99142c97f778e453267a3d98422a21635e787b2f
Orlando Miramontes <frizt.orlando@gmail.com>
2017-05-03 19:28:41 -0500

    Added solarized-mate-terminal

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 printf "#!/bin/bash\n\
 \n\
 source $CURR_DIR/bashrc\n" > ~/.bashrc
+
+mate-terminal --help &>/dev/null
+
+if [ $? -eq 0 ]; then
+    pushd $CURR_DIR/sources/solarized-mate-terminal
+    bash solarized-mate.sh
+    popd
+fi
