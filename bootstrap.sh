22ea19c15717047458ffe77e39ee48faa2a16af4
Miramontes, Orlando <maorlan@amazon.com>
2018-04-30 21:11:14 -0700

    Small fix to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ INSTALLER_FILE=rc_install.sh
 if [ -f "$TARGET_BASHRC" ]; then
     BACKUP="$TARGET_BASHRC.back.$RANDOM"
     echo "Current $TARGET_BASHRC will be backed up in $BACKUP"
-    #cp $TARGET_BASHRC $BACKUP
+    cp $TARGET_BASHRC $BACKUP
     echo "Removing $TARGET_BASHRC"
     rm $TARGET_BASHRC
 fi
